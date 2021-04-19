8d613e1069b1c4e00ac8170db6918f9a85d8473f
root <cagojeiger@naver.com>
2021-04-19 05:44:54 +0000

    add add factory pattern 1

--- test_driven_development/chapter_1/main.py
+++ test_driven_development/chapter_1/main.py
@@ -1,9 +1,41 @@
 from __future__ import annotations
 
+from typing import Optional
 
-class Dollar:
-    def __init__(self, amount: int):
-        self.amount: int = amount
 
+class Money:
+    def __init__(self, amount: int, currency: str):
+        self._amount = amount
+        self._currency = currency
+
+    def __eq__(self, obj: Money) -> bool:
+        if not isinstance(obj, Money):
+            raise TypeError()
+
+        is_same_class = self.__class__ == obj.__class__
+        is_same_amount = self._amount == obj._amount
+        return is_same_amount and is_same_class
+
+    def times(self, multiplier: int) -> Money:
+        raise NotImplementedError()
+
+    def currency(self) -> str:
+        return self._currency
+
+    @staticmethod
+    def dollar(amount: int) -> Dollar:
+        return Dollar(amount, "USD")
+
+    @staticmethod
+    def franc(amount: int) -> Franc:
+        return Franc(amount, "CHF")
+
+
+class Dollar(Money):
     def times(self, multiplier: int) -> Dollar:
-        return Dollar(self.amount * multiplier)
+        return Money.dollar(self._amount * multiplier)
+
+
+class Franc(Money):
+    def times(self, multiplier: int) -> Franc:
+        return Money.franc(self._amount * multiplier)
--- test_driven_development/chapter_1/test.py
+++ test_driven_development/chapter_1/test.py
@@ -1,15 +1,29 @@
 import unittest
 
-from .main import Dollar
+from .main import Dollar, Franc, Money
 
 
 class TestCase(unittest.TestCase):
-    def test_main(self):
-        five = Dollar(5)
-        product = five.times(2)
-        self.assertEqual(10, product.amount)
-        product = five.times(3)
-        self.assertEqual(15, product.amount)
+    def test_multiplication(self):
+        five = Money.dollar(5)
+        self.assertEqual(Money.dollar(10), five.times(2))
+        self.assertEqual(Money.dollar(15), five.times(3))
+
+    def test_franc_multiplication(self):
+        five = Money.franc(5)
+        self.assertEqual(Money.franc(10), five.times(2))
+        self.assertEqual(Money.franc(15), five.times(3))
+
+    def test_equality(self):
+        self.assertTrue(Money.dollar(5) == Money.dollar(5))
+        self.assertFalse(Money.dollar(5) == Money.dollar(6))
+        self.assertTrue(Money.franc(5) == Money.franc(5))
+        self.assertFalse(Money.franc(5) == Money.franc(6))
+        self.assertFalse(Money.franc(5) == Money.dollar(5))
+
+    def test_currency(self):
+        self.assertEqual("USD", Money.dollar(1).currency())
+        self.assertEqual("CHF", Money.franc(1).currency())
 
 
 if __name__ == "__main__":
