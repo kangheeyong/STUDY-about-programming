7253d3303695fbd2235c162936c868674ac8e33c
root <cagojeiger@naver.com>
2021-04-19 07:07:12 +0000

    add concept metaphor

--- test_driven_development/chapter_1/main.py
+++ test_driven_development/chapter_1/main.py
@@ -1,20 +1,66 @@
 from __future__ import annotations
 
+from enum import Enum
+from typing import Dict, Union
 from dataclasses import dataclass
+
+
+class Currency(Enum):
+    USD = "usd"
+    CHF = "chf"
 
 
 @dataclass(eq=True, frozen=True)
 class Money:
-    amount: int
-    currency: str
+    amount: float
+    currency: Currency
+
+    def plus(self, added: Money) -> Sum:
+        return Sum(self, added)
 
     def times(self, multiplier: int) -> Money:
         return Money(self.amount * multiplier, self.currency)
 
+    def reduced(self, bank: Bank, currency: Currency) -> Money:
+        rate = bank.rate(self.currency, currency)
+        return Money(self.amount / rate, currency)
+
     @staticmethod
     def dollar(amount: int) -> Money:
-        return Money(amount, "USD")
+        return Money(amount, Currency.USD)
 
     @staticmethod
     def franc(amount: int) -> Money:
-        return Money(amount, "CHF")
+        return Money(amount, Currency.CHF)
+
+
+@dataclass
+class Sum:
+    augend: Money
+    addend: Money
+
+    def reduced(self, bank: Bank, currency: Currency) -> Money:
+        amount = self.augend.amount + self.addend.amount
+        return Money(amount, currency)
+
+
+@dataclass(eq=True, frozen=True, unsafe_hash=True)
+class Pair:
+    _from: Currency
+    _to: Currency
+
+
+class Bank:
+    rates: Dict[Pair, int] = {}
+
+    def reduced(self, source: Union[Sum, Money], currency: Currency) -> Money:
+        return source.reduced(self, currency)
+
+    def addRate(self, _from: Currency, _to: Currency, rate: int):
+        self.rates[Pair(_from=_from, _to=_to)] = rate
+
+    def rate(self, _from: Currency, _to: Currency) -> int:
+        if _from == _to:
+            return 1
+
+        return self.rates[Pair(_from=_from, _to=_to)]
--- test_driven_development/chapter_1/test.py
+++ test_driven_development/chapter_1/test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from .main import Money
+from .main import Money, Bank, Currency, Sum
 
 
 class TestCase(unittest.TestCase):
@@ -20,8 +20,41 @@ class TestCase(unittest.TestCase):
         self.assertFalse(Money.franc(5) == Money.dollar(5))
 
     def test_currency(self):
-        self.assertEqual("USD", Money.dollar(1).currency)
-        self.assertEqual("CHF", Money.franc(1).currency)
+        self.assertEqual(Currency.USD, Money.dollar(1).currency)
+        self.assertEqual(Currency.CHF, Money.franc(1).currency)
+
+    def test_simpe_addition(self):
+        five = Money.dollar(5)
+        _sum = five.plus(five)
+        bank = Bank()
+        reduced = bank.reduced(_sum, Currency.USD)
+        self.assertEqual(Money.dollar(10), reduced)
+
+    def test_plus_returns_sum(self):
+        five = Money.dollar(5)
+        _sum = five.plus(five)
+        self.assertEqual(five, _sum.augend)
+        self.assertEqual(five, _sum.addend)
+
+    def test_reduce_sum(self):
+        _sum = Sum(Money.franc(5), Money.franc(2))
+        bank = Bank()
+        reduced = bank.reduced(_sum, Currency.USD)
+        self.assertEqual(Money.dollar(7), reduced)
+
+    def test_reduce_money(self):
+        bank = Bank()
+        reduced = bank.reduced(Money.dollar(1), Currency.USD)
+        self.assertEqual(Money.dollar(1), reduced)
+
+    def test_reduce_money_different_curruncy(self):
+        bank = Bank()
+        bank.addRate(Currency.CHF, Currency.USD, 2)
+        result = bank.reduced(Money.franc(2), Currency.USD)
+        self.assertEqual(Money.dollar(1), result)
+
+    def test_identity_rate(self):
+        self.assertEqual(1, Bank().rate(Currency.USD, Currency.USD))
 
 
 if __name__ == "__main__":
